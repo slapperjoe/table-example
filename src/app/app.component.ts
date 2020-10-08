5b4fce457693b2cf85fc7d99c7fa9ba21cc49e1a
Mark Richards <markr@live.com>
2020-10-08 12:31:04 +1100

    remove car

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Car } from '../models/car';
+
 import { Money, MoneyData } from '../models/money';
 import { Observable } from 'rxjs';
 import { DeserializeArray } from 'cerializr';
@@ -19,11 +19,7 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
-    // this.cars$ = this.http.get('/assets/car.json').pipe(
-    //   map((res: JsonArray) => DeserializeArray(res, Car)),
-    //   tap(res => console.log(res))
-    // );
+  ngOnInit(): void {    
 
     this.monies$ = this.http.get('/assets/money.json').pipe(
       map((res: JsonArray) => DeserializeArray(res, MoneyData)),
